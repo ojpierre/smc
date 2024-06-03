0f77fcd3077bac683af30770303d011aba0d7fb5
Peter Ouma <pierreouma@gmail.com>
2024-06-03 15:49:30 +0300

    updates 03/06/2024

--- home.php
+++ home.php
@@ -16,27 +16,33 @@ if (!isset($_SESSION['user_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - SMC</title>
     <link rel="stylesheet" href="static/css/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <header>
-                <h1>Social Media Campaigns (SMC)</h1>
+        <h1>Social Media Campaigns (SMC)</h1>
         <nav>
             <ul>
-            <li><a href="home.php">Home</a></li>
-            <li><a href="information.php">Information</a></li>
-            <li><a href="popular_apps.php">Popular Apps</a></li>
-            <li><a href="parents_help.php">Parents Help</a></li>
-            <li><a href="livestreaming.php">Livestreaming</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="legislation.php">Legislation</a></li>
-            <li><?php if (!isset($_SESSION['user_id'])) : ?>
-                <a href="register.php">Register</a>
-                <a href="login.php">Login</a>
-            <?php else : ?>
-                <a href="logout.php">Logout</a>
-            <?php endif; ?>
-            </li>
+                <li><a href="home.php">Home</a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Pages</a>
+                    <div class="dropdown-content">
+                        <a href="information.php">Information</a>
+                        <a href="popular_apps.php">Popular Apps</a>
+                        <a href="parents_help.php">Parents Help</a>
+                        <a href="livestreaming.php">Livestreaming</a>
+                        <a href="legislation.php">Legislation</a>
+                    </div>
+                </li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><?php if (!isset($_SESSION['user_id'])) : ?>
+                    <a href="register.php">Register</a>
+                    <a href="login.php">Login</a>
+                <?php else : ?>
+                    <a href="logout.php">Logout</a>
+                <?php endif; ?>
+                </li>
             </ul>
         </nav>
     </header>
@@ -85,24 +91,24 @@ if (!isset($_SESSION['user_id'])) {
             <a href="register.php">Register Now</a>
         </section>
     </main>
-    <section class="socials"> <h2>SOCIAL MEDIA APPS</h2>
-        
-            <div class="social">
-                <a href="https://www.facebook.com" target="_blank">
-                    <img src="static/images/facebook-icon.png" alt="facebook">
-                </a>
-            </div>
-            <div class="social">
-                <a href="https://www.twitter.com" target="_blank">
-                    <img src="static/images/twitter-icon.png" alt="twitter">
-                </a>
-            </div>
-            <div class="social">
-                <a href="https://www.instagram.com" target="_blank">
-                    <img src="static/images/instagram-icon.png" alt="instagram">
-                </a>
-            </div>
-        </section>
+    <section class="socials">
+        <h2>SOCIAL MEDIA APPS</h2>
+        <div class="social">
+            <a href="https://www.facebook.com" target="_blank">
+                <img src="static/images/facebook-icon.png" alt="facebook">
+            </a>
+        </div>
+        <div class="social">
+            <a href="https://www.twitter.com" target="_blank">
+                <img src="static/images/twitter-icon.png" alt="twitter">
+            </a>
+        </div>
+        <div class="social">
+            <a href="https://www.instagram.com" target="_blank">
+                <img src="static/images/instagram-icon.png" alt="instagram">
+            </a>
+        </div>
+    </section>
     <footer>
         <p>&copy; 2024 Social Media Campaigns Ltd. All rights reserved.</p>
     </footer>
